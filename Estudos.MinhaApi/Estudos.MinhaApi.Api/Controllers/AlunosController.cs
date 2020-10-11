fdc5d4fba191561537812a4963f2d4953d2b5cf4
ariele-fatima <ariele_afd@yahoo.com.br>
2020-10-10 22:37:25 -0300

    AB#352 - Criando o controller e os primeiros endpoints

---
+++ Estudos.MinhaApi/Estudos.MinhaApi.Api/Controllers/AlunosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls.WebParts;
+using Estudos.Comum.Repositorios.Interfaces;
+using Estudos.MinhaApi.AcessoDados.Entity.Context;
+using Estudos.MinhaApi.Dominio;
+using Estudos.MinhaApi.Repositorios.Entity;
+
+namespace Estudos.MinhaApi.Api.Controllers
+{
+    public class AlunosController : ApiController
+    {
+        private IRepositorioEstudos<Aluno, int> _repositorioAlunos 
+            = new RepositorioAlunos(new MinhaApiDbContext());
+
+        public IEnumerable<Aluno> Get()
+        {
+            return _repositorioAlunos.Selecionar();
+        }
+
+        public Aluno Get(int? id)
+        {
+            return _repositorioAlunos.SelecionarPorId(id.Value);
+        }
+
+    }
+}
